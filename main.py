1a04b7da36e991a9a09a4a0e3f93201af51b1ad4
Engel Alexandre <alexandre.engel@insa-rouen.fr>
2024-07-19 22:30:46 +0200

    #1 Ajout des fonctionnalités en rapport avec les fiches RP

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ class Hoxie(commands.Bot):
         super().__init__(command_prefix="!", intents=discord.Intents.all())
 
     async def setup_hook(self) -> None:
+        await bot.load_extension("ficheRP")
         await self.tree.sync() # Pour gérer les commandes slash
 
     async def on_ready(self) -> None:
         print("Allumage du bot...")
 
 bot = Hoxie()
-
+    
 if __name__ == "__main__":
     bot.run(os.getenv('DISCORDTOKEN'))
